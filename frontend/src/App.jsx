b53dd6a976a4acfd9be8e2aa0d27a5404b1646ff
Mithalesh Kumar Bind <mk2018me@gmail.com>
2025-02-11 00:34:35 +0530

    tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,10 @@ function App() {
 
   return (
     <>
-      <h1>Hey there! </h1>
+      <h1 className='text-5xl font-bold text-center mt-20'> hello from React</h1>
+      <div className='flex flex-col items-center mt-10'>
+         <p className='text-lg'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae nostrum in praesentium commodi quos aspernatur ea ipsum rerum labore accusamus eum quis voluptates voluptatem, consequatur cumque voluptate excepturi iste provident.</p>
+         </div>
     </>
   )
 }
