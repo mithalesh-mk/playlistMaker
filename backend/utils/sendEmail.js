0c6acdb8fc389f0acc8a9b3625c935a59b17ce82
Mithalesh Kumar Bind <mk2018me@gmail.com>
2025-02-25 02:42:41 +0530

    template for email done

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,6 +1,6 @@
 const nodemailer = require("nodemailer");
 
-const sendEmail = async (to, subject, text) => {
+const sendEmail = async (to, subject, html) => {
   const transporter = nodemailer.createTransport({
     service: "Gmail",
     auth: {
@@ -13,7 +13,7 @@ const sendEmail = async (to, subject, text) => {
     from: process.env.EMAIL_USER,
     to,
     subject,
-    text,
+    html,
   };
 
   await transporter.sendMail(mailOptions);
