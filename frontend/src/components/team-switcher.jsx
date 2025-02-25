d3e869e9eb570e35cd9ab455396e3c904a88a361
Mithalesh Kumar Bind <mk2018me@gmail.com>
2025-02-26 01:02:23 +0530

    logo added and loading changed

--- frontend/src/components/team-switcher.jsx
+++ frontend/src/components/team-switcher.jsx
@@ -30,12 +30,17 @@ export function TeamSwitcher() {
               size="lg"
               >
               <div
-                className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-                <TvMinimalPlay size={'16'}/>
+                className="flex aspect-square size-8 items-center justify-center rounded-lg text-sidebar-primary-foreground">
+                <svg width="1030" height="981" viewBox="0 0 1030 981" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M809 240L667 576.5C560.6 415.46 447 307.067 403.5 273C529.1 423.8 693.167 735.5 759.5 872.5L1030 240H809Z" fill="#FF0000"/>
+                <path d="M712.5 981H492C238.4 301.8 58.3333 44 0 0C399.2 235.2 641.333 752 712.5 981Z" fill="#FF0000"/>
+                <path d="M795.5 788L676.5 554.5L713 467.5L834 698L795.5 788Z" fill="#A60000"/>
+                </svg>
+
               </div>
-              <div className="grid flex-1 text-left text-sm leading-tight">
-                <span className="truncate font-semibold">
-                  PlayListMaker
+              <div className="grid flex-1 text-left text-md leading-tight">
+                <span className="truncate font-semibold ml-1 mt-2">
+                  Vessel
                 </span>
                 
               </div>
