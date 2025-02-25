dc09b961b01161d846b1ffbb894122754973966b
Sahilsajar <sahilsajar13890@gmail.com>
2025-02-26 02:13:23 +0530

    Profile Designing

--- frontend/src/app/profile/Profile.jsx
+++ frontend/src/app/profile/Profile.jsx
@@ -7,8 +7,10 @@ import React from "react";
 
 const Profile = () => {
   const [profileCurrComponent, setCurComponent] = useState(<Details></Details>);
+  const [flag, setFlag] = useState(true);
 
   const changeToDetails = (flag) => {
+    setFlag(flag);
     if (flag) {
       setCurComponent(<Details></Details>);
     } else {
@@ -20,22 +22,33 @@ const Profile = () => {
       <section id="profile" className="px-[1rem]">
         <div className="max-w-[1024px] mx-auto ">
           <h1 className="text-5xl my-[2rem]">Profile</h1>
-          <div className="flex gap-[1rem] border-b-2 border-grey">
-            <button
+          <div className="flex justify-around">
+            <div
+              className={
+                flag
+                  ? "w-1/2 flex justify-center border-gray-100 border-r border-l border-t text-xl dark:bg-gray-700 bg-gray-200"
+                  : "w-1/2 flex justify-center border-gray-100 border-r border-l border-t text-xl"
+              }
               onClick={() => {
                 changeToDetails(true);
               }}
             >
               Details
-            </button>
-            <button
+            </div>
+            <div
+              className={
+                flag
+                  ? "w-1/2 flex justify-center border-gray-100 border-r border-l border-t text-xl "
+                  : "w-1/2 flex justify-center border-gray-100 border-r border-l border-t text-xl dark:bg-gray-700 bg-gray-200"
+              }
               onClick={() => {
                 changeToDetails(false);
               }}
             >
               Setting
-            </button>
+            </div>
           </div>
+          <div className="h-1 w-full bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 rounded-full mb-4"></div>
           {profileCurrComponent}
         </div>
       </section>
--- frontend/src/app/profile/Setting.jsx
+++ frontend/src/app/profile/Setting.jsx
@@ -4,6 +4,7 @@ import { useAuth } from "@/userContext/AuthProvider";
 import { UserRoundPen } from "lucide-react";
 import { Link } from "react-router-dom";
 import PopUp from "./PopUp";
+import axiosInstance from "@/axiosInstance";
 function Setting() {
   const { user } = useAuth();
   const { username, email, profilePic } = user;
@@ -12,43 +13,59 @@ function Setting() {
 
   const handleUsernameChange = (e) => setUsername(e.target.value);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     // Implement your update logic here
+    try {
+      const res = await axiosInstance.put("/auth/change-username", {
+        newUsername: newusername,
+      });
+      console.log(res.data);
+    } catch (error) {
+      alert(error);
+      return error;
+    }
   };
 
   return (
     <div className="max-w-[1024] mx-auto p-4 shadow-md">
       <h2 className="text-xl font-bold mb-4">Profile Settings</h2>
-      <div className="relative max-w-[400px]">
-        <img
-          src={profilePic}
-          className="w-full h-auto max-h-[350px] max-w-[350px]"
-        ></img>
-        <Link to={"/choose-avatar"}>
-          <UserRoundPen className="absolute bottom-[2rem] right-[7rem] dark:bg-black rounded-sm bg-white" />
-        </Link>
-      </div>
-      <form onSubmit={handleSubmit} className="max-w-[640px]">
-        <div className="mb-4">
-          <label className="block">Change Username:</label>
-          <input
-            type="text"
-            value={newusername}
-            onChange={handleUsernameChange}
-            className=" border p-2 rounded dark:bg-black dark:text-white w-full max-w-[250px]"
-            placeholder="Enter new username"
-          />
+      <div className="flex justify-between items-center  flex-col lg:flex-row  gap-2 lg:gap-0">
+        <div className="relative max-w-[400px] w-full md:w-1/2">
+          <img
+            src={profilePic}
+            className="w-full h-auto max-h-[350px] max-w-[350px]"
+          ></img>
+          <Link to={"/choose-avatar"}>
+            <div className="bg-gray-700 text-white px-4 py-2 rounded hover:bg-gray-900 w-fit mx-auto md:mx-0">
+              Change Profile
+            </div>
+          </Link>
         </div>
-        <button
-          type="submit"
-          className="bg-gray-700 text-white px-4 py-2 rounded hover:bg-gray-900"
-        >
-          Update
-        </button>
-      </form>
-
-      <PopUp></PopUp>
+        <div className="w-full md:w-1/2 flex justify-center flex-col mt-5 md:mt-0">
+          <form onSubmit={handleSubmit} className="max-w-[640px]">
+            <div className="mb-4 flex justify-between flex-col md:flex-row">
+              <label className="text-xl pt-1">Change Username </label>
+              <input
+                type="text"
+                value={newusername}
+                onChange={handleUsernameChange}
+                className=" border p-2 rounded dark:bg-black dark:text-white w-full max-w-[300px] h-9 my-auto"
+                placeholder="Enter new username"
+              />
+            </div>
+            <div className="flex md:justify-end">
+              <button
+                type="submit"
+                className="bg-gray-700 text-white px-4 py-2 rounded hover:bg-gray-900"
+              >
+                Save
+              </button>
+            </div>
+          </form>
+          <PopUp></PopUp>
+        </div>
+      </div>
     </div>
   );
 }
