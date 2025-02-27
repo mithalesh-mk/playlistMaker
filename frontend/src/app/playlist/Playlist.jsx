7877e223c0ad23171c1eba08032f9fb1f3e2f903
Sahilsajar <sahilsajar13890@gmail.com>
2025-02-28 02:12:45 +0530

    Delete video

--- frontend/src/app/playlist/Playlist.jsx
+++ frontend/src/app/playlist/Playlist.jsx
@@ -53,8 +53,7 @@ const Playlist = () => {
   const [isBookmark, setBookmark] = useState(false);
   const [noOfLikes, setNoOfLikes] = useState(0);
   const [noOfDislike, setNoOfDislikes] = useState(0);
-  const [newOrder, setNewOrder] = useState([])
-
+  const [newOrder, setNewOrder] = useState([]);
 
   const [data, setData] = useState({
     title: "",
@@ -84,14 +83,9 @@ const Playlist = () => {
         videos: data.videos,
         isOwner: data.isOwner,
       });
-<<<<<<< HEAD
 
       setNoOfLikes(data.likes.length);
       setNoOfDislikes(data.dislikes.length);
-
-=======
-        console.log(data,'playlistData')
->>>>>>> acc4964bf790dcd78218c59a239523a1c1f0f3c6
       fetchVideos();
     } catch (error) {
       console.error(error);
@@ -254,32 +248,40 @@ const Playlist = () => {
       }
 
       const newVideos = arrayMove(prev.videos, oldIndex, newIndex);
-  
-      console.log("New Order:", newVideos.map((v) => v._id)); // Debugging
-  
+
+      console.log(
+        "New Order:",
+        newVideos.map((v) => v._id)
+      ); // Debugging
+
       setNewOrder(newVideos);
-  
+
       return { ...prev, videos: newVideos };
     });
   };
 
   useEffect(() => {
     if (newOrder.length === 0) return;
-   updateVideoOrder(newOrder);
-    
-}, [newOrder]);
-  
-  console.log(data.videos)
+    updateVideoOrder(newOrder);
+  }, [newOrder]);
+
+  console.log(data.videos);
 
   // Function to send updated order to backend
   const updateVideoOrder = async (newVideos) => {
     try {
       const newOrder = newVideos.map((video) => video._id);
-      console.log(newOrder)
-      const response = await axiosInstance.put(`/playlist/updateOrder/${playlistId}`, { newOrder });
-  
+      console.log(newOrder);
+      const response = await axiosInstance.put(
+        `/playlist/updateOrder/${playlistId}`,
+        { newOrder }
+      );
+
       if (response.data.success === false) {
-         toast({ description: "Failed to update order", variant: "destructive" })
+        toast({
+          description: "Failed to update order",
+          variant: "destructive",
+        });
       }
     } catch (error) {
       console.error("Error updating playlist order:", error);
