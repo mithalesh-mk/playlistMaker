ce0454887ef01c0d97b622eb583b44e17b25a39e
Vis7044 <singhvishal7044@gmail.com>
2025-04-02 01:01:35 +0530

    added oauth

--- frontend/src/ProtecedRoute.jsx
+++ frontend/src/ProtecedRoute.jsx
@@ -1,16 +1,20 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "./userContext/AuthProvider";
-import { useEffect } from "react";
 
 const ProtectedRoute = ({ children }) => {
-  const { user, loading, handleLogout } = useAuth(); // Ensure logout function is available
+  const { user, loading } = useAuth(); // No need to destructure `handleLogout` here
+
   const token = localStorage.getItem("auth_token");
-  if (!token) {
-    handleLogout();
-  }
+  console.log("user:", user, "token:", token);
+
   if (loading) return <p>Loading...</p>;
 
-  return user ? children : <Navigate to="/login" />;
+  // If no token or no user, redirect to login
+  if (!token || !user) {
+    return <Navigate to="/login" />;
+  }
+
+  return children;
 };
 
 export default ProtectedRoute;
